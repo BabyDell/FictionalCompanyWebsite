dfafceeb2b174d4e88e5022e20ad0bb6602a13eb
BabyDell <A.Java.Coder7@gmail.com>
2024-11-16 14:45:51 -0800

    hot fix

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -127,7 +127,7 @@ export default function ContactUs() {
                     </div>
                   </div>
                 </CardContent>
-              </Card>
+              </Card> 
             </div>
           </div>
         </div>
